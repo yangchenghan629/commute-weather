106518ae7ccd369372f3c51c34ed37e5bcef8eaa
Ych <johnny062995@gmail.com>
2026-06-15 17:25:46 +0800

    update config

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+import os
+
+CWA_API_KEY = os.environ.get("CWA_API_KEY", "")
+GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY", "")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
+LINE_USER_ID = os.environ.get("LINE_USER_ID", "")
